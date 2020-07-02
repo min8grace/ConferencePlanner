09ef271186cbd144b7a825b1161e624edbb99c24
min8grace <56316476+min8grace@users.noreply.github.com>
2020-07-01 22:15:28 -0700

    Add project files.

---
+++ FrontEnd/Middleware/SkipWelcomeAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class SkipWelcomeAttribute : Attribute
+    {
+
+    }
+}
---
+++ FrontEnd/Services/AdminService.cs
@@ -0,0 +1,54 @@
+﻿using FrontEnd.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Services
+{
+    public class AdminService : IAdminService
+    {
+        //private readonly IdentityDbContext _dbContext;
+
+        //private bool _adminExists;
+
+        //public AdminService(IdentityDbContext dbContext)
+        //{
+        //    _dbContext = dbContext;
+        //}
+        private readonly IServiceProvider _serviceProvider;
+
+        private bool _adminExists;
+
+        public AdminService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public async Task<bool> AllowAdminUserCreationAsync()
+        {
+            if (_adminExists)
+            {
+                return false;
+            }
+            else
+            {
+                using (var scope = _serviceProvider.CreateScope())
+                {
+                    var dbContext = scope.ServiceProvider.GetRequiredService<IdentityDbContext>();
+
+                    if (await dbContext.Users.AnyAsync(user => user.IsAdmin))
+                    {
+                        // There are already admin users so disable admin creation
+                        _adminExists = true;
+                        return false;
+                    }
+
+                    // There are no admin users so enable admin creation
+                    return true;
+                }
+            }
+        }
+    }
+}
